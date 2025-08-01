d607b4e2673547bb225b157f1a5c33677bc3126c
JuliusBourbon <naherrrrr@gmail.com>
2025-08-01 17:20:58 +0700

    Add browser sync

---
+++ bs-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  proxy: "localhost/My-Resto", // sesuaikan dengan nama folder di XAMPP
+  files: ["**/*.php", "**/*.css", "**/*.js", "**/*.html"],
+  port: 3000,
+  open: true,
+  notify: false
+}
---
+++ view/kokiMenu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Koki - List Menu</title>
+</head>
+<body class="">
+    <div class="bg-amber-700">
+        <h1>My Resto</h1>
+    </div>
+</body>
+</html>
