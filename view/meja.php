06e55e71567c82367aec9e82ea32ca00c22b3cb1
JuliusBourbon <naherrrrr@gmail.com>
2025-08-02 17:12:37 +0700

    Update meja

---
+++ view/meja.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <style>
+        body {
+            font-family: 'Poppins', sans-serif;
+        }
+    </style>
+    <link href="../output.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Asap+Condensed:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Manrope:wght@200..800&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Meja</title>
+</head>
+<body class="bg-gray-50 pt-[118px]">
+    <div class="flex flex-col">
+        <nav class="fixed top-0 left-0 right-0 z-50 bg-white border-b border-gray-200">
+            <div class="relative flex items-center justify-center px-10 py-4">
+                <div class="absolute left-10 top-1/2 -translate-y-1/2">
+                    <div class="w-18 h-18">
+                        <img src="../img/myresto_icon.jpg" alt="Logo" class="w-full h-full object-contain">
+                    </div>
+                </div>
+                <span class="text-2xl font-bold">MyResto</span>
+                <div class="absolute right-10 top-1/2 -translate-y-1/2">
+                    <div class="w-10 h-10 rounded-full bg-blue-700 flex items-center justify-center text-white cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                    </div>
+                </div>
+            </div>
+             <div class="px-10">
+                <div class="flex items-center gap-6 pt-1">
+                    <a href="#" class="py-2 text-blue-700 font-semibold border-b-2 border-blue-700">Pesanan</a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="flex items-center">
+            <!-- <div class=""> -->
+            <div class="flex flex-col gap-30">
+                <h1 class="bg-gray-300 py-2 px-5 rotate-[-90deg]">Kasir</h1>
+                <h1 class="bg-gray-300 py-2 px-5 rotate-[-90deg]">Pintu</h1>
+            </div>
+            <!-- </div> -->
+            <div class="w-full grid grid-cols-5 gap-5">
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full flex flex-col justify-center items-center">
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                    <div class="w-full flex items-center justify-center gap-5">
+                        <div class="w-30 h-30 rounded-full bg-gray-300 flex items-center justify-center">
+                            <h1 class="text-center">1 <br> Tersedia</h1>
+                        </div>
+                    </div>
+                    <div class="w-24 h-24 rounded-full flex items-center justify-center gap-5">
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                        <div class="w-8 h-8 bg-gray-300">
+                            <h1></h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex bg-amber-400 justify-center items-center">
+                <h1>Daftar Meja</h1>
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
